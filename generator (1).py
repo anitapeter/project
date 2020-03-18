934b9a5b353fb561ea25ba963f03201c1bd417aa
anitapeter <38970993+anitapeter@users.noreply.github.com>
2020-03-18 11:53:52 +0530

    Add files via upload

---
+++ generator (1).py
@@ -0,0 +1,25 @@
+from tagsList import Tags
+
+def getTags(list_of_keywords):
+	tags = Tags['common']
+
+	for key in list_of_keywords:
+		for tag in Tags[key]:
+		 tags.append(tag)
+
+	return list(set(tags))
+
+def hashes(list_of_tags):
+	hashTagString = ""
+
+	for tag in list_of_tags:
+	 hashTagString += " #" + tag
+
+	return hashTagString
+
+if __name__ == '__main__':
+  import sys
+  with open ("detected.txt", "r") as fileHandler: 
+    for line in fileHandler:
+      print(line.strip())
+      print (hashes(getTags(line)))
